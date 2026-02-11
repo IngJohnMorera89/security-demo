e721e3757efaa5c59a97a20e60a4e34a1455d320
Ing John Morera Barrera <johnmorera.89@gmail.com>
2026-02-10 23:20:26 -0500

    confiuracion  y generacion JWT

--- src/main/java/com/devsenior/jmorera/securitydemo/service/AuthServiceImpl.java
+++ src/main/java/com/devsenior/jmorera/securitydemo/service/AuthServiceImpl.java
@@ -1,20 +1,25 @@
 package com.devsenior.jmorera.securitydemo.service;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsenior.jmorera.securitydemo.mapper.UserMapper;
 import com.devsenior.jmorera.securitydemo.model.dto.LoginRequestDto;
 import com.devsenior.jmorera.securitydemo.model.dto.LoginResponseDto;
 import com.devsenior.jmorera.securitydemo.model.dto.RegisterRequestDto;
+import com.devsenior.jmorera.securitydemo.model.entity.RoleEntity;
 import com.devsenior.jmorera.securitydemo.repository.RoleRepository;
 import com.devsenior.jmorera.securitydemo.repository.UserRepository;
+import com.devsenior.jmorera.securitydemo.util.JwtUtils;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,9 +32,11 @@ public class AuthServiceImpl implements AuthService {
     private final RoleRepository roleRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtils jwtUtils;
 
 
     @Override
+    @Transactional(readOnly = true)
     public LoginResponseDto login(LoginRequestDto credentials) {
         try{
         var auth =new UsernamePasswordAuthenticationToken(credentials.username(), credentials.password());
@@ -42,8 +49,15 @@ public class AuthServiceImpl implements AuthService {
         var user = userRepository.findById(credentials.username()).get();
         
 
-        var token = String.format("%s:-:%s", user.getUsername(), user.getEmail());
-        return new LoginResponseDto(token, "CUSTOM"); 
+        var token = jwtUtils.generateToken(credentials.username(), Map.of(
+                "email", user.getEmail(),
+                "name" , user.getName(),
+                "hire_date",user.getHireDate().format(DateTimeFormatter.BASIC_ISO_DATE),
+                "roles", user.getRoles().stream()
+                        .map(RoleEntity::getName)
+                        .toList()));
+     
+        return new LoginResponseDto(token, "JWT"); 
      }      
     @Override
     public void register(RegisterRequestDto info) {
